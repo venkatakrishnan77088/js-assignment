369c7439cd07087bde9d563c33c7c0d32e240f61
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2022-12-02 22:58:41 +0530

    altering the program logic

--- dom/js/js.js
+++ dom/js/js.js
@@ -1,17 +1,16 @@
-var x=createElement("form");
- 
- 
  function getAdd(){
-	  const num1 = Number(document.getElementById("val1").value);
+	  num1 = Number(document.getElementById("val1").value);
          
         // Fetch the value of input with id val2
-        const num2 = Number(document.getElementById("val2").value);
-        for(i=num1;num1<=num2;i=i*3){
+       num2 = Number(document.getElementById("val2").value);
+        for(i=1;i<=num2;i++){
+			m=num1*i
+			console.log(m)
          
         // Displays the result in paragraph using dom
-        document.getElementById("result").innerHTML = "multiple of 3: " + i;
+        document.getElementById("result").innerHTML = "multiple of 3: " + m;
          
         // Changes the color of paragraph tag with red
-        document.getElementById("result").style.color = "red";
+        //document.getElementById("result").style.color = "red";
 		}
  }
