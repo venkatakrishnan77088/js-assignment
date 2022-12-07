81503f2159ca470a2cd6a3a046a4ee90adcfd3d8
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2022-12-07 07:57:55 +0530

    Adding css

--- dom/js/js.js
+++ dom/js/js.js
@@ -2,40 +2,73 @@ Button=document.createElement("Button")
 Button.setAttribute("onclick","start();");
 Button.innerHTML="click Me"
 document.body.appendChild(Button);
-//w=document.createElement("div");
+//document.body.background-color="#00FFFF";
 
 
+
+
+body.setAttribute("id","f2")
+body.setAttribute("background-color","#00FFFF")
 function start(){
    startButton=document.createElement("Button");
    startButton.setAttribute("onclick","multipleof3();"); 
    startButton.innerHTML="start";
    document.body.appendChild(startButton)
-  /* q=document.createElement("Button");
-   q.setAttribute("onclick","run();");
-   document.body.appendChild(q)
-   q.innerHTML="run";*/
+  
  
 }
  function multipleof3(){
     var inputButton1 = document.createElement("INPUT");
     inputButton1.setAttribute("type", "text");
     inputButton1.setAttribute("placeholder", "enter the number:");
-  
+	inputButton1.setAttribute("style","background-color:white")
+    inputButton1.style.border="10px solid #48C9B0";
+	inputButton1.style.width="30%"
+	inputButton1.style.borderRadius="5px"
+	inputButton1.style.height="70px"
+	inputButton1.style.marginLeft="35%"
     inputButton1.setAttribute("id","n1");
+	document.write("<br>");
+document.write("<br>");
+document.write("<br>");
   
    var inputButton2 = document.createElement("INPUT");
    inputButton2.setAttribute("type", "text");
+   
    inputButton2.setAttribute("placeholder", "enter the range:");
    inputButton2.setAttribute("id","n2");
+   inputButton2.setAttribute("style","background-color:white")
+ 
+   
    document.body.appendChild(inputButton1);
+   document.write("<br>");
+   document.write("<br>");
+   document.write("<br>");
+   inputButton2.style.width="30%"
+   inputButton2.style.height="70px"
+   inputButton2.style.borderRadius="5px"
+   inputButton2.style.marginLeft="35%"
+   inputButton2.style.border="10px solid #F8C471";
    document.body.appendChild(inputButton2);
    runButton=document.createElement("Button");
    runButton.setAttribute("onclick","run();");
+   runButton.style.marginLeft="35%"
+   runButton.style.height="70px"
+   runButton.style.width="20%"
+   runButton.style.borderRadius="5px"
+   runButton.style.backgroundColor="#F5CBA7"
+   document.write("<br>");
+   document.write("<br>");
+   document.write("<br>");
    document.body.appendChild(runButton)
    runButton.innerHTML="run";
- 
+ div1=document.createElement("div")
+ div1.style.marginLeft="37%"
+ div1.style.marginTop="10px"
+ document.body.appendChild(div1);
   
  }
+ 
  function run(){
   num1 = Number(document.getElementById("n1").value);
          
@@ -44,8 +77,9 @@ function start(){
         for(i=1;i<=num2;i++){
 			m=num1*i
 			console.log(m)
-			document.write(m+"</br>");
-         
+			
+			//document.write(m+"</br>");
+             div1.innerHTML+=m+"</br>"
         // Displays the result in paragraph using dom
        
          
