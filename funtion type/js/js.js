97b750c8634b09daf386db8f737423c7df81f73b
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2022-12-14 17:20:58 +0530

    alptering code

---
+++ funtion type/js/js.js
@@ -0,0 +1,32 @@
+//function with argument and with return type
+/*
+function venkat(f,h){
+	return f+h;
+}
+console.log(venkat(2,6))*/
+
+//function with argument and with whithout return type
+
+/*
+function venkat(f,h){
+	console.log(f+h);
+}
+venkat(2,6)*/
+
+//function without argument and with  return type
+
+/*function fun(){
+	return '3';
+}
+d=fun()
+console.log(d)
+*/
+
+//function without argument and without return type
+/*function fun(){
+	console.log("hello");
+}
+fun()
+
+
+*/
